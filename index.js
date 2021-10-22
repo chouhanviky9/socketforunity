ed8d8a8fb2c53215d449d4b1eb34a0f86d77909b
chouhanviky9 <chouhanviky9@gmail.com>
2021-10-22 18:43:19 +0530

    added

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const wss = new WebSocket.Server({ port: 8080 },()=>{
     console.log('server started')
 })
 wss.on('connection', function connection(ws) {
-   ws.on('message', (data) => {
-      console.log('data received \n %o',data)
-      ws.send(data);
-   })
+    console.log('connection');
+    ws.on('message', function incoming(data, isBinary) {
+        console.log(data.toString(),isBinary);
+        wss.clients.forEach(function each(client) {
+          if (client !== ws && client.readyState === WebSocket.OPEN) {
+              console.log(data,isBinary);
+            client.send("true");
+         
+          }
+        });
+      });
 })
 wss.on('listening',()=>{
    console.log('listening on 8080')
