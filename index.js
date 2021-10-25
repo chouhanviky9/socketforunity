189e6920518443286ca624a921463784febd7b95
chouhanviky9 <chouhanviky9@gmail.com>
2021-10-25 12:37:33 +0530

    updated

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const PORT = 8080;
 const server = express()
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+  server.get('/get', (req, res) => {
+    res.send("hello from this side");
+  })
   const { Server } = require('ws');
 
 const wss = new Server({ server });
