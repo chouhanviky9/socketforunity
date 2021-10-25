502986a7f675e71a5aa3b678611e357e96f8bd90
chouhanviky9 <chouhanviky9@gmail.com>
2021-10-25 12:40:51 +0530

    flag

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const PORT = 8080;
 
-const server = express()
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const app = express();
 
-  server.get('/get', (req, res) => {
-    res.send("hello from this side");
-  })
-  const { Server } = require('ws');
+app.get('/', (req, res) => {
+  res.send("hello world");
+})
 
-const wss = new Server({ server });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
+app.listen(PORT, ()=>{
+  console.log('listening on port 8080');
+})
