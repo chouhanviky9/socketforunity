8b5ed766c3d84fbcd5bdf3df78d4d8d69f3deca3
Vikas kumar <chouhanviky9@gmail.com>
2021-10-27 12:08:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(data, isBinary) {
         console.log(data.toString(),isBinary);
         wss.clients.forEach(function each(client) {
+            console.log(client);
 //           if (client !== ws && client.readyState === WebSocket.OPEN) {
               console.log(data.toString(),isBinary);
             client.send("true");
